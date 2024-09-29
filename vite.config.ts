caec32a4356e275a32832255e8e3d53ee0b9ae97
Maplefallin <2107522784@qq.com>
2024-09-29 23:21:14 +0800

    cqx

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,6 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
-import { resolve } from 'path'
+import path, { resolve } from 'path'
 
 export default defineConfig({
   plugins: [vue()],
@@ -9,7 +9,14 @@ export default defineConfig({
   server: {             
     host: '0.0.0.0',	
     // port: 8080,      
-    open: true
+    open: true,
+    proxy:{
+      '/api':{
+        target:"http://127.0.0.1:4523/m1/5091463-4753819-default",//真正浏览器代理发送的端口
+        changeOrigin:true,
+        rewrite:(path) =>path.replace(/^\/api/,'')//将api替换为空串      
+      }
+    }
   },
   resolve:{   
     //别名配置，引用src路径下的东西可以通过@如：import Layout from '@/layout/index.vue'
